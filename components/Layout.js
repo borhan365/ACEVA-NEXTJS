64669e69a8f8513680c14441382874dd66b9422b
borhan365 <info.mdborhan@gmail.com>
2023-03-22 00:08:51 +0600

    added chatbot

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import Header from './Header'
+import Chatbot from './chatbot/Chatbot'
 import Footer from './Footer'
-import { client } from '../lib/client'
-import {groq} from 'next-sanity'
-import { loadData } from '../pages/api/post'
+import Header from './Header'
+
 
 function Layout({headers, footers, children}) {
   return (
     <>
       <Header headers={headers} />
       {children}
+      <Chatbot />
       <Footer footers={footers} />
     </>
   )
